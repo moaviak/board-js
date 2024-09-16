0081ae293bcda60ca5d7d76977389fbfa68b535f
moavia <mmoavia2002@gmail.com>
2024-09-16 20:06:59 +0500

    card actions implemented (copy link, update title, delete board)

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
 import { useAuth } from "@clerk/nextjs";
+import { MoreHorizontal } from "lucide-react";
 import { formatDistanceToNow } from "date-fns";
 
+import { Actions } from "@/components/actions";
 import { Skeleton } from "@/components/ui/skeleton";
 
 import { Footer } from "./footer";
@@ -44,6 +46,11 @@ export const BoardCard = ({
         <div className="relative flex-1 bg-amber-50">
           <Image src={imageUrl} alt={title} fill className="object-fill" />
           <Overlay />
+          <Actions id={id} title={title} side="right">
+            <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+              <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
+            </button>
+          </Actions>
         </div>
         <Footer
           isFavorite={isFavorite}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { Toaster } from "@/components/ui/sonner";
+import { ModalProvider } from "@/providers/modal-provider";
 import { ConvexClientProvider } from "@/providers/convex-client-provider";
 
 import "./globals.css";
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ConvexClientProvider>
           {children}
+          <ModalProvider />
           <Toaster />
         </ConvexClientProvider>
       </body>
